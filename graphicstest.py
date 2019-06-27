e01f7b07aaab0131d90c1611681b4c9f0e2c7fc3
Charlie <melvins_fan@hotmail.com>
2019-06-27 16:30:44 +0100

    Added a correct Oscillator Circuit + option to modify Antenna and have learned how to use enumerate to add values from different lists that use same index as the one belonging to the choice a user made from a pre=defined list

--- graphicstest.py
+++ graphicstest.py
@@ -1,9 +1,10 @@
 from graphics import *
 import math
+import win32api  #REALLY useful (install with pip install PyWin32 but import as win32api) allows WIndows style alert boxes
 Volt=0
 Amp=0
 Ohm=0
-win = GraphWin(width = 400, height = 400) # create a window
+win = GraphWin(width = 600, height = 600) # create a window
 win.setCoords(0, 0, 10, 10) # set the coordinates of the window; bottom left is (0, 0) and top right is (10, 10)
 
 label=Text(Point(5,1),"input source voltage")
@@ -76,7 +77,7 @@ def Oscillator():
     Capinput.draw(win)
     win.getMouse()
     Capacitance=float(Capinput.getText())
-    Caplabel2=Text(Point (3, 3.75), str(Capacitance) +" uF")
+    Caplabel2=Text(Point (5, 6), str(Capacitance) +" uF")
     Caplabel2.draw(win)
     Capchoice.undraw()
     Capinput.undraw()
@@ -88,16 +89,16 @@ def Oscillator():
     Inductance=float(Indinput.getText())
     Indchoice.undraw()
     Indinput.undraw()
-    Indlabel2=Text(Point(3,8), str(Inductance)+"mH")
+    Indlabel2=Text(Point(0.5,6), str(Inductance)+"mH")
     Indlabel2.draw(win)
     CapInd=Capacitance*Inductance*10**-6
     RootCI=math.sqrt(CapInd)
     Resonantfreq=1/2*math.pi*RootCI
     print("resonant freq is " + str(Resonantfreq))
-    Cap=Rectangle(Point(2.5,4.5),Point (3.5,5.5))# first pair of vals = position of lower LEFT corner, second = pos of lower RIGHT
+    Cap=Rectangle(Point(3.5,5.5),Point (4.5,6.5))# first pair of vals = position of lower LEFT corner, second = pos of lower RIGHT
     Cap.setFill("yellow")
     Cap.draw(win)
-    Inductor=Rectangle(Point(2.5, 6.5), Point (3.5, 7.5))
+    Inductor=Rectangle(Point(1.5, 5.5), Point (2.5, 6.5))
     Inductor.setFill("blue")
     Inductor.draw(win)
     Userchoice=Text(Point(5,9), "Do you want to change values?")
@@ -129,7 +130,71 @@ def Oscillator():
 
     #Inductor=
     
-Oscillator()  
+Oscillator()
+def Antenna():
+        print("test")
+        def Antchoice():
+        #data taken from http://www.antenna-theory.com/basics/bandwidth.php
+             Antwin = GraphWin(title="Antenna types", width = 500, height = 500) # create a window
+             Antwin.setCoords(0, 0, 10, 10)
+             Windlabel=Text(Point (5, 9.5), "Antenna types")
+             Windlabel.setStyle('bold')
+             Windlabel.draw(Antwin)
+             Patchlabel=Text(Point(5,8.5), "Patch - Center Freq:1000MHz, Freq Range:985-1015Mhz")
+             Dilabel=Text(Point(5,6.5), "Dipole - Center Freq:1000MHz, Freq Range: 960-1040Mhz")
+             Hornlabel=Text(Point(5,4.5),"Horn - Center Freq:10000Mhz, Freq Range:154-1848Mhz")
+             Spirallabel=Text(Point(5, 2.5), "Spiral - Center Freq:1000Mhz, Freq Range: 95-1900MHz")
+             Patchlabel.draw(Antwin)
+             Dilabel.draw(Antwin)
+             Hornlabel.draw(Antwin)
+             Spirallabel.draw(Antwin)
+             Choicelabel=Text(Point(5, 1), "Please choose an Antenna type - \"Patch\", \"Dipole\", \"Horn\", \"Spiral\"")
+             Choicelabel.setFace("arial")
+             Choicelabel.setStyle("italic")
+             Userentry=Entry(Point(5,0.25),10)
+             Userentry.draw(Antwin)
+             Choicelabel.draw(Antwin)
+             Antwin.getMouse()
+             choice=Userentry.getText()
+             choices=( "Patch","Dipole", "Horn", "Spiral")
+             Anthighs=(1015, 1040, 1848, 1900)
+             Antlows=(985, 960, 154, 95)
+             if choice in choices:
+                        for idx, val in enumerate(choices):   #REALLY useful function (enumerate(list)) enables you to get INDEX of item from a list - can then be used to extract OTHER values from other lists with same index!
+                            if val==choice:
+                                print(idx)
+                                i=idx
+                        print(i)
+                        Antwin.close()
+                        Anttype=choice
+                        AntCentFreq=1000
+                        Antlow=Antlows[i]
+                        Anthigh=Anthighs[i]
+                        Antlabel=Text(Point(2.5, 9.9),Anttype + ": "+ str(Antlow)+" Mhz - "+str(Anthigh) +" Mhz")
+                        Antlabel.draw(win)
+          #   elif choice == "Dipole":
+           #           Antwin.close()
+            #          Anttype="Dipole"
+             #         AntCentFreq=1000
+              #        Antlow=960
+               #       Anthigh=1040
+                #      Antlabel=(Point(2.5, 9.9), "Dipole: "+ str(Antlow)+" Mhz - "+str(Anthigh) + " Mhz")
+                 #     Antlabel.draw(win)
+             else:
+                        win32api.MessageBox(0, 'Please choose a valid option', 'Incorrect Entry')
+                        Antwin.close()
+                      
+                        Antchoice()
+            
+        Antchoice()
+        Wire=Line (Point (2, 7), Point(2, 9.5))
+        Wire.draw(win)
+        Wire2=Line(Point(1.5,9.5), Point (2.5,9.5))
+        Wire2.draw(win)
+        Wire3=Line(Point(1,9.7), Point(3, 9.7))
+        Wire3.draw(win)
+        
+Antenna()
 Wire3=Line(Point(x3,y3), Point (x4,y4))
 Wire3.draw(win)
 Square4=Rectangle(Point(7.5,4),Point(8.5,5))
